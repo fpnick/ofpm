582c76df26d838e39fa33deaec9341b0fe1d62bf
Fabian Nick <fp.nick@gmail.com>
2017-07-28 14:30:59 +0200

    Copied script for conditionnumbers from old oFPM.

---
+++ conditionnumbers.m
@@ -0,0 +1,23 @@
+
+x=0.02:0.01:0.1; 
+condition = zeros(length(x),1);
+size = zeros(length(x),1);
+eigmax = zeros(length(x),1);
+eigmin = zeros(length(x),1);
+
+
+parfor i=1:length(x)
+    x(i)
+    [A,rhs,sol]=ofpm(x(i),0);
+    condition(i) = condest(A);
+    size(i) = length(A);
+    eigmin(i) = eigs(A,1,'sm');
+    eigmax(i) = eigs(A,1);
+    
+    A_norm = A./diag(A);
+    condition_norm(i) = condest(A_norm);
+    eigmin_norm(i) = eigs(A,1,'sm');
+    eigmax_norm(i) = eigs(A,1);
+    
+    matrizen{i} = A;
+end
