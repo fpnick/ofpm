867effc31a62106764522d2c8188a1a8c6cc78b8
Fabian Nick <fp.nick@gmail.com>
2017-07-28 14:41:34 +0200

    Adjust conditionnumbers script

--- conditionnumbers.m
+++ conditionnumbers.m
@@ -1,5 +1,5 @@
 
-x=0.02:0.01:0.1; 
+x=0.03:0.01:0.1; 
 condition = zeros(length(x),1);
 size = zeros(length(x),1);
 eigmax = zeros(length(x),1);
@@ -8,7 +8,7 @@ eigmin = zeros(length(x),1);
 
 parfor i=1:length(x)
     x(i)
-    [A,rhs,sol]=ofpm(x(i),0);
+    [A,rhs,sol]=ofpm_oo(x(i),0);
     condition(i) = condest(A);
     size(i) = length(A);
     eigmin(i) = eigs(A,1,'sm');
