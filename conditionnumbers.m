9a5a54a8bfe7fd8d6d2b282a54261085dd52ba6d
Fabian Nick <fp.nick@gmail.com>
2017-07-28 20:12:20 +0200

    A lot of labeling in the conditionnumbers script.

--- conditionnumbers.m
+++ conditionnumbers.m
@@ -1,9 +1,10 @@
 
-x=0.02:0.01:0.1; 
+x=0.07:0.01:0.1; 
 condition = zeros(length(x),1);
 size = zeros(length(x),1);
 eigmax = zeros(length(x),1);
 eigmin = zeros(length(x),1);
+diagdom = zeros(length(x),1);
 
 
 parfor i=1:length(x)
@@ -13,19 +14,37 @@ parfor i=1:length(x)
     size(i) = length(A);
     eigmin(i) = eigs(A,1,'sm');
     eigmax(i) = eigs(A,1);
+    diagdom(i) = measureDiagDom(A);
     
     A_norm = A./diag(A);
     condition_norm(i) = condest(A_norm);
     eigmin_norm(i) = eigs(A,1,'sm');
     eigmax_norm(i) = eigs(A,1);
+    diagdom(i) = measureDiagDom(A_norm);
     
     matrizen{i} = A;
 end
 
 figure;
-plot(size,condition,size,condition_norm)
+plot(size,condition,'s',size,condition_norm,'s');
+title("Condition Numbers");
+legend("Unscaled","Normalized");
+legend('Location','northwest');
+xlabel("Matrix Size");
 figure;
-plot(size,eigmin,size,eigmax)
+semilogy(size,eigmin,'v',size,eigmax,'^');
+title("Eigenvalues of Unscaled Systems (absolute values)");
+legend("Min","Max");
+legend('Location','southwest');
+xlabel("Matrix Size");
 figure;
-plot(size,eigmin_norm,size,eigmax_norm)
+semilogy(size,eigmin_norm,'v',size,eigmax_norm,'^');
+title("Eigenvalues of Normalized Systems (absolute values)");
+legend("Min","Max");
+legend('Location','southwest');
+xlabel("Matrix Size");
+figure;
+plot(size,diagdom,'x');
+title("Diagonal Dominance");
+xlabel("Matrix Size");
 
