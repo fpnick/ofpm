9c491c61f278a28c50f665e7bb112fcb153cae8f
Fabian Nick <fp.nick@gmail.com>
2017-08-18 14:17:14 +0200

    Refactoring.

--- Multigrid.m
+++ Multigrid.m
@@ -5,45 +5,54 @@ classdef Multigrid < handle
     properties
 
       solver % Note that solver contains the complete hierarchy + matrices
-      SMOOTHER = 1    % 1: Gauss-Seidel
-      RESTRICTION = 1 % 1: Inclusion
-      INTERPOLATION = 1 % 1: Weighed based on distance
-      nPreSmooth = 1
-      nPostSmooth = 1
+
+      % Parameters
+      SMOOTHER      = 1  % 1: Gauss-Seidel
+      RESTRICTION   = 1  % 1: Inclusion
+      INTERPOLATION = 1  % 1: Weighed based on distance
+      nPreSmooth    = 1  % n: Number of pre-smoothing steps
+      nPostSmooth   = 1  % n: Number of post-smoothing steps
     end
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
     methods
+
       function obj = Multigrid(solver)
          obj.solver = solver;
       end
 
       function solution = solve(obj,u,tol)
-
-         res = norm( obj.solver.matrices{1}*u-obj.solver.rhss{1}, 2);
-         res0 = res;
-         fprintf('Initial residual: %1.3e\n', res);
-         tol_abs = res * tol;
+         res        = norm( obj.solver.matrices{1}*u-obj.solver.rhss{1}, 2);
+         res0       = res;
+         tol_abs    = res * tol;
          iterations = 0;
+
+         fprintf('Initial residual: %1.3e\n', res);
+         
          while ( res > tol_abs )
-            u = obj.cycle( 1, u, obj.solver.rhss{1});
-            res = norm( obj.solver.matrices{1}*u-obj.solver.rhss{1}, 2);
+            u          = obj.cycle( 1, u, obj.solver.rhss{1});
+            res        = norm( obj.solver.matrices{1}*u-obj.solver.rhss{1}, 2);
             iterations = iterations + 1;
+
             fprintf('Residual after iteration %i: %1.3e\n', iterations,res);
          end
 
          solution = u;
-         rho = (res/res0)^(1/iterations);
-         fprintf('rho = %1.3f\n', rho);
+         rho      = (res/res0)^(1/iterations);
 
+         fprintf('rho = %1.3f\n', rho);
       end
+
     end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
     methods (Access=private)
 
       function u = cycle( obj, level, u, f )
          if ( level == obj.solver.hierarchy.depth )
             % Coarsest level => direct solve
-            % disp('Direct solve');
             u = obj.solver.matrices{level} \ f;
          else
             u              = obj.smooth( obj.solver.matrices{level}, u, f, obj.nPreSmooth);
@@ -55,7 +64,6 @@ classdef Multigrid < handle
             u              = u - correctionFine;
             u              = obj.smooth( obj.solver.matrices{level}, u, f, obj.nPostSmooth);
          end
-         
       end
 
       function u = smooth (obj,A, u, f, iter)
@@ -72,39 +80,38 @@ classdef Multigrid < handle
 
       function R = restrict( obj, resVec, level)
          NCoarse = obj.solver.hierarchy.pointclouds{level+1}.N;
-         R = zeros( NCoarse, 1);
+         R       = zeros( NCoarse, 1);
 
          if ( obj.RESTRICTION == 1 )
             for i=1:NCoarse
                R(i) = resVec( obj.solver.hierarchy.coarse2fine{level+1}(i) );
             end
          end
-
       end
 
       function r = interpolate( obj, correction, level)
          % level refers to the level that is interpolated FROM
          % NOTE: Setup of interpolation should be done ONCE, not in every
          % iteration!
-         NFine = obj.solver.hierarchy.pointclouds{level-1}.N;
+         NFine       = obj.solver.hierarchy.pointclouds{level-1}.N;
          fine2coarse = obj.solver.hierarchy.fine2coarse{level};
-         r = zeros (NFine, 1);
+         r           = zeros (NFine, 1);
 
          if ( obj.INTERPOLATION == 1 )
             for i=1:NFine
                if ( fine2coarse(i) == 0 )
                   neighbourList = obj.solver.hierarchy.pointclouds{level-1}.neighbourLists{i};
-                  distanceList = obj.solver.hierarchy.pointclouds{level-1}.distanceLists{i};
-                  nNeighbours = length(neighbourList);
+                  distanceList  = obj.solver.hierarchy.pointclouds{level-1}.distanceLists{i};
+                  nNeighbours   = length(neighbourList);
 
                   sumDistances = 0;
                   for j=1:nNeighbours
-                     if  ( fine2coarse(neighbourList(j)) ~= 0 )
+                     if ( fine2coarse(neighbourList(j)) ~= 0 )
                         sumDistances = sumDistances + distanceList(j);
                      end
                   end
                   for j=1:nNeighbours
-                     if  ( fine2coarse(neighbourList(j)) ~= 0 )
+                     if ( fine2coarse(neighbourList(j)) ~= 0 )
                         r(i) = r(i) + correction(fine2coarse(neighbourList(j))) * (distanceList(j)/sumDistances);
                      end
                   end
@@ -114,5 +121,7 @@ classdef Multigrid < handle
             end
          end
       end
+      
     end
+
 end %class
