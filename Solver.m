c521a1277301f34988bcf0a83c65ec0c4ded0b31
Fabian Nick <fp.nick@gmail.com>
2017-11-23 08:33:54 +0100

    Solve for 10 digits.

--- Solver.m
+++ Solver.m
@@ -46,7 +46,7 @@ classdef Solver < handle
           else
              mg = Multigrid(obj);
              rng(1);
-             [obj.sol,rho] = mg.solve(zeros(obj.pointcloud.N,1),10^(-15));
+             [obj.sol,rho] = mg.solve(zeros(obj.pointcloud.N,1),10^(-10));
           end
           toc
 
