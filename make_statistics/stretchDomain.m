e0528a6738456fc129ce9c6df0e5427fd06d93b1
Fabian Nick <fp.nick@gmail.com>
2017-12-05 16:19:39 +0100

    Disable a lot of stuff in stretchDomain test.

--- make_statistics/stretchDomain.m
+++ make_statistics/stretchDomain.m
@@ -2,7 +2,7 @@
 addpath('..')
 
 X=1:1:11
-H=0.05
+H=0.08
 
 condition = zeros(length(X),1);
 size = zeros(length(X),1);
@@ -13,57 +13,57 @@ diagdom = zeros(length(X),1);
 
 parfor i=1:length(X)
     X(i)
-    [A,rhs,sol,pointcloud,rho]=ofpm_oo(H,0,0,X(i),1,0);
-    condition(i) = condest(A);
+    [A,rhs,sol,pointcloud,rho,iter_needed]=ofpm_oo(H,0,0,X(i),1,0);
+    % condition(i) = condest(A);
     size(i) = length(A);
-    eigmin(i) = eigs(A,1,'sm');
-    eigmax(i) = eigs(A,1);
-    diagdom(i) = measureDiagDom(A);
+    % eigmin(i) = eigs(A,1,'sm');
+    % eigmax(i) = eigs(A,1);
+    % diagdom(i) = measureDiagDom(A);
     
-    A_norm = A./diag(A);
-    condition_norm(i) = condest(A_norm);
-    eigmin_norm(i) = eigs(A_norm,1,'sm');
-    eigmax_norm(i) = eigs(A_norm,1);
-    diagdom(i) = measureDiagDom(A_norm);
+    % A_norm = A./diag(A);
+    % condition_norm(i) = condest(A_norm);
+    % eigmin_norm(i) = eigs(A_norm,1,'sm');
+    % eigmax_norm(i) = eigs(A_norm,1);
+    % diagdom(i) = measureDiagDom(A_norm);
     
-    A_scaledBoundary = scaleBoundary(A,pointcloud.ibound);
-    condition_scaledBoundary(i) = condest(A_scaledBoundary);
-    eigmin_scaledBoundary(i) = eigs(A_scaledBoundary,1,'sm');
-    eigmax_scaledBoundary(i) = eigs(A_scaledBoundary,1);
-    diagdom_scaledBoundary(i) = measureDiagDom(A_scaledBoundary);
+    % A_scaledBoundary = scaleBoundary(A,pointcloud.ibound);
+    % condition_scaledBoundary(i) = condest(A_scaledBoundary);
+    % eigmin_scaledBoundary(i) = eigs(A_scaledBoundary,1,'sm');
+    % eigmax_scaledBoundary(i) = eigs(A_scaledBoundary,1);
+    % diagdom_scaledBoundary(i) = measureDiagDom(A_scaledBoundary);
     
-    matrizen{i} = A;
+    % matrizen{i} = A;
 end
 
-figure;
-plot(size,condition,'o',size,condition_norm,'s',size,condition_scaledBoundary,'x');
-title(sprintf('Condition Numbers; H=%3.2f',H));
-legend('Original','Normalized','Scaled Boundary');
-legend('Location','northwest');
-xlabel('Matrix Size');
-figure;
-semilogy(size,eigmin,'v',size,eigmax,'^');
-title(sprintf('Eigenvalues of Original Systems (absolute values); H=%3.2f',H));
-legend("Min","Max");
-legend('Location','southwest');
-xlabel("Matrix Size");
-figure;
-semilogy(size,eigmin_norm,'v',size,eigmax_norm,'^');
-title(sprintf('Eigenvalues of Normalized Systems (absolute values); H=%3.2f',H));
-legend('Min','Max');
-legend('Location','southwest');
-xlabel('Matrix Size');
+% figure;
+% plot(size,condition,'o',size,condition_norm,'s',size,condition_scaledBoundary,'x');
+% title(sprintf('Condition Numbers; H=%3.2f',H));
+% legend('Original','Normalized','Scaled Boundary');
+% legend('Location','northwest');
+% xlabel('Matrix Size');
+% figure;
+% semilogy(size,eigmin,'v',size,eigmax,'^');
+% title(sprintf('Eigenvalues of Original Systems (absolute values); H=%3.2f',H));
+% legend("Min","Max");
+% legend('Location','southwest');
+% xlabel("Matrix Size");
+% figure;
+% semilogy(size,eigmin_norm,'v',size,eigmax_norm,'^');
+% title(sprintf('Eigenvalues of Normalized Systems (absolute values); H=%3.2f',H));
+% legend('Min','Max');
+% legend('Location','southwest');
+% xlabel('Matrix Size');
 % figure;
 % plot(X,diagdom,'x');
 % title("Diagonal Dominance");
 % xlabel("Geometry Length");
-figure;
-semilogy(size,eigmin_scaledBoundary,'v',size,eigmax_scaledBoundary,'^');
-title(sprintf('Eigenvalues of System with Scaled Boundaries (absolute values); H=%3.2f',H));
-legend('Min','Max');
-legend('Location','southwest');
-xlabel('Matrix Size');
-figure;
-plot(size,rho,'x');
-title("Convergence rates of Original system");
-xlabel("Matrix Size");
+% figure;
+% semilogy(size,eigmin_scaledBoundary,'v',size,eigmax_scaledBoundary,'^');
+% title(sprintf('Eigenvalues of System with Scaled Boundaries (absolute values); H=%3.2f',H));
+% legend('Min','Max');
+% legend('Location','southwest');
+% xlabel('Matrix Size');
+% figure;
+% plot(size,rho,'x');
+% title("Convergence rates of Original system");
+% xlabel("Matrix Size");
